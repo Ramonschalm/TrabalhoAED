5c50430aa5ce336f7b74a05b44d5a7e08cb8dcc5
Ramonschalm <102772283+Ramonschalm@users.noreply.github.com>
2024-06-15 16:19:16 -0300

    Add files via upload

---
+++ TrabalhoAED/Curso.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+
+namespace TrabalhoAED
+{
+    public class Curso
+    {
+        public string Nome { get; set; }
+
+        public int Vagas { get; set; }
+
+        public double NotaCorte { get; set; }
+
+        public Candidato[] Aprovados { get; set; }
+
+        public Fila ListaEspera { get; set; }
+
+        public int VagasPreenchidas { get; set; }
+
+        public Curso(string nome, int vagas)
+        {
+            Nome = nome;
+            Vagas = vagas;
+            Aprovados = new Candidato[vagas];
+            ListaEspera = new Fila(10);
+            NotaCorte = 0;
+            VagasPreenchidas = 0;
+
+        }
+    }
+}
